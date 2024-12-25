4716dbb062f880512959ea657b05bd6bf4b3b08a
Maksim <velikiymaksimys2281488@yandex.ru>
2024-12-25 14:37:47 +0300

    lab7_fix

--- lab7/loadDishes.js
+++ lab7/loadDishes.js
@@ -7,30 +7,4 @@ async function loadDishes() {
 loadDishes()
 
 
-// обращаемся к API fetch или XMLHttpRequest.
-// получаем ответ с блюдами
-// на основе ответа строим карточки
-
-// должны отработать json который получаем
-
-
-
-// Функция для загрузки блюд с сервера
-// async function loadDishes() {
-//     fetch('https://edu.std-900.ist.mospolytech.ru/labs/api/dishes')
-//         .then(response => response.json())
-//         .then(data => {
-//             dishes = data;
-//             displayDishes('soup', dishesContainers.soup);
-//             displayDishes('main-course', dishesContainers.mainCourse);
-//             displayDishes('salad', dishesContainers.salads);
-//             displayDishes('drink', dishesContainers.beverage);
-//             displayDishes('dessert', dishesContainers.dessert);
-//             updateSelectedDishes();
-//         })
-//         .catch(error => console.error('Ошибка загрузки данных:', error));
-// }
-
-// Загрузка блюд при загрузке страницы
-
 
